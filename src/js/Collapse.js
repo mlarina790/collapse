2e1164b00cf543f745492a12aa4583900cd5b199
mlarina790 <mlarina790@gmail.com>
2023-03-27 16:54:18 +0300

    исправление ошибок

--- src/js/Collapse.js
+++ src/js/Collapse.js
@@ -1,16 +1,15 @@
 export default class Collapse {
-    constructor(container) {
-      this.container = container;
-      this.button = this.container.querySelector('.collapse_button');
-    }
-  
-    init() {
-      this.button.addEventListener('click', (event) => this.getTextBlock(event));
-    }
-  
-    getTextBlock(event) {
-      event.preventDefault();
-      this.container.querySelector('.collapse_container').classList.toggle('active');
-    }
+  constructor(container) {
+    this.container = container;
+    this.button = this.container.querySelector('.collapse_button');
   }
-  
+
+  init() {
+    this.button.addEventListener('click', (event) => this.getTextBlock(event));
+  }
+
+  getTextBlock(event) {
+    event.preventDefault();
+    this.container.querySelector('.collapse_container').classList.toggle('active');
+  }
+}
